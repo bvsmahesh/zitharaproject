be34dce35a923f5cc2b44832c80549f7cf4c632a
bvsmahesh <151134910+bvsmahesh@users.noreply.github.com>
2024-03-03 10:35:26 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react'
+import New from './New';
+function App() {
+  return (
+    <div className="App">
+      <h1 className="m-2 text-bg-info text-center" >React search</h1>
+      <New/>
+    </div>
+  );
+}
+
+export default App;
